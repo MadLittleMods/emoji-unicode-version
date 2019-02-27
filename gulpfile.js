e3f793de10b17ab76ba071de3dbece84feb5b7d4
Christophe Coevoet <stof@notk.org>
2019-02-27 11:37:29 +0100

    Drop gulp-eslint to run gulp
    The `gulp lint` script does not exit with a failure exit code when eslint
    fails, making it useless for usage in a scripted usage (as done in the
    prepublish script for instance).
    The `npm run lint` script now uses eslint directly instead of using gulp as
    a wrapper.
    Using eslint directly also allows using its autofixing feature, by running
    it as `npm run lint -- --fix`.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,9 @@
 const gulp = require('gulp');
 const Promise = require('bluebird');
 const fs = require('fs-extra');
-const eslint = require('gulp-eslint');
 const outputFile = Promise.promisify(fs.outputFile);
 
 const generateEmojiUnicodeVersionMap = require('./tasks/emoji-unicode-version-map');
-
-
-gulp.task('lint-js', () => {
-	return gulp.src(['**/*.js', '!node_modules/**'])
-		.pipe(eslint())
-		// eslint.format() outputs the lint results to the console.
-    .pipe(eslint.format());
-});
-
-
-gulp.task('lint', gulp.series(
-	'lint-js'
-));
-
 
 gulp.task('generate-emoji-unicode-version-map', () => {
 	return outputFile('./emoji-unicode-version-map.json', JSON.stringify(generateEmojiUnicodeVersionMap(), null, '\t'));
@@ -30,6 +15,5 @@ gulp.task('build', gulp.series(
 
 
 gulp.task('default', gulp.series(
-	'lint',
 	'build'
 ));
